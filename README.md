762f2110999cca548a61f3ac2b2f0c5bbe4fe2fc
Lee Goddard <leegee@gmail.com>
2025-06-17 12:16:09 +0200

    Tunings

--- README.md
+++ README.md
@@ -6,4 +6,7 @@ just started learning the instrument.
 This package is an attempt at a better engine for discovering chords that aims to 
 find Manouche chords as well as more standard tuning.
 
+It currently provides a number of tuning options for guitar, banjo, and mandola/mandolin.
+
 [https://leegee.github.io/guitar-chords-2](https://leegee.github.io/guitar-chords-2)
+
--- src/lib/tunings.ts
+++ src/lib/tunings.ts
@@ -38,7 +38,7 @@ export const tuningOptions: TuningOption[] = [
         },
     },
     {
-        label: "Mandola/Mandolin",
+        label: "Mandola/Mandolin/Tenor Banjo",
         tuning: {
             4: "G",
             3: "D",
@@ -47,7 +47,61 @@ export const tuningOptions: TuningOption[] = [
         },
     },
     {
-        label: "Standard 7-string BEADGBE",
+        label: "Banjo Standard Plectrum",
+        tuning: {
+            4: "C",
+            3: "G",
+            2: "B",
+            1: "D",
+        },
+    },
+    {
+        label: "Banjo Open G",
+        tuning: {
+            4: "D",
+            3: "G",
+            2: "B",
+            1: "G",
+        },
+    },
+    {
+        label: "Banjo Double C",
+        tuning: {
+            4: "C",
+            3: "G",
+            2: "C",
+            1: "D",
+        },
+    },
+    {
+        label: "Banjo C",
+        tuning: {
+            4: "C",
+            3: "G",
+            2: "B",
+            1: "D",
+        },
+    },
+    {
+        label: "Banjo D",
+        tuning: {
+            4: "D",
+            3: "F♯",
+            2: "A",
+            1: "D",
+        },
+    },
+    {
+        label: "Banjo G Modal",
+        tuning: {
+            4: "D",
+            3: "G",
+            2: "C",
+            1: "D",
+        },
+    },
+    {
+        label: "Guitar Standard 7-string BEADGBE",
         tuning: {
             7: "B",
             6: "E",
