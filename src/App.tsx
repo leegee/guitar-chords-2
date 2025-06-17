81406d054c6b355075c220009990be28e29977ff
Lee Goddard <leegee@gmail.com>
2025-06-17 10:37:45 +0200

    Extrapolate

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { createSignal } from 'solid-js';
 import { type ConstraintProfile, generateCandidateShapes } from './lib/chord-finder';
 import ChordDiagram from './ChordDiagram';
+import RootSelector from './RootSelector';
 import './style.css';
 import { chromaticScale, getMajorChordNotes } from './lib/notes';
 
@@ -27,14 +28,7 @@ export default function App() {
         <main>
             <h1>Major Chord Shapes</h1>
 
-            <label>
-                Select root note:
-                <select value={rootNote()} onChange={(e) => setRootNote(e.currentTarget.value)}>
-                    {chromaticScale.map(note => (
-                        <option value={note}>{note}</option>
-                    ))}
-                </select>
-            </label>
+            <RootSelector rootNote={rootNote()} setRootNote={setRootNote} options={chromaticScale} />
 
             <section class="chord-diagrams">
                 {chordShapes().map((shape) => (
